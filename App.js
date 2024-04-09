8e258d28665e10f1ab6b956956fdbff4ce573767
Anhar <anhar_tasman@yahoo.com>
2024-04-10 03:55:27 +0700

    Activity Indicator

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  ScrollView,
-  Button,
-  Pressable,
-  Modal,
-} from "react-native";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
 import { useState } from "react";
 const logoImg = require("./assets/adaptive-icon.png");
 
@@ -18,7 +9,9 @@ export default function App() {
     //flex : 1 willl make view occupy the entire screen
     //onRequestClose : called when user press back button on the device
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <StatusBar backgroundColor="lightgreen" style="dark" />
+      <ActivityIndicator />
+      <ActivityIndicator size="large" color="midnightblue" />
+      <ActivityIndicator size="large" color="midnightblue" animating={false} />
     </View>
   );
 }
