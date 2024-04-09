22956e00b6e971fcde5dfa1e42059ad1360a421c
Anhar <anhar_tasman@yahoo.com>
2024-04-10 03:51:51 +0700

    StatusBar

--- App.js
+++ App.js
@@ -17,26 +17,8 @@ export default function App() {
   return (
     //flex : 1 willl make view occupy the entire screen
     //onRequestClose : called when user press back button on the device
-    //presentationStyle only affects on the iOS and not on the Android
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <ScrollView>
-        <Button
-          title="Press"
-          onPress={() => setIsModalVisible(true)}
-          color="midnightblue"
-        />
-        <Modal
-          visible={isModalVisible}
-          onRequestClose={() => setIsModalVisible(false)}
-          animationType="slide"
-          presentationStyle="formSheet"
-        >
-          <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
-            <Text>Modal content</Text>
-            <Button title="Close" onPress={() => setIsModalVisible(false)} />
-          </View>
-        </Modal>
-      </ScrollView>
+      <StatusBar backgroundColor="lightgreen" style="dark" />
     </View>
   );
 }
