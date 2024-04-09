5364e96cbc106b6da9a6e8c3df939b086606025d
Anhar <anhar_tasman@yahoo.com>
2024-04-09 22:00:29 +0700

    Scroll View

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     //flex : 1 willl make view occupy the entire screen
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      {/* <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      <Image
-        source={{ uri: "https://picsum.photos/300/300" }}
-        style={{ width: 300, height: 300 }}
-      /> */}
-      <ImageBackground source={logoImg} style={{ flex: 1 }}>
-        <Text>Image Text</Text>
-      </ImageBackground>
+      <ScrollView>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </Text>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </ScrollView>
     </View>
   );
 }
