6964bb017f1b1c80a5d166184e01c40dedb7a2c0
Anhar <anhar_tasman@yahoo.com>
2024-04-10 02:50:37 +0700

    Pressable

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   Image,
   ScrollView,
   Button,
+  Pressable,
 } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
@@ -13,12 +14,27 @@ export default function App() {
   return (
     //flex : 1 willl make view occupy the entire screen
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button
-        title="Press"
-        onPress={() => console.log("Button pressed")}
-        color="midnightblue"
-        disabled
-      />
+      <ScrollView>
+        <Button
+          title="Press"
+          onPress={() => console.log("Button pressed")}
+          color="midnightblue"
+        />
+        <Pressable onPress={() => console.log("image pressed")}>
+          <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        </Pressable>
+        <Pressable onPress={() => console.log("text pressed")}>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </Text>
+        </Pressable>
+      </ScrollView>
     </View>
   );
 }
