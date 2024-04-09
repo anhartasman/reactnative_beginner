378e2ee4922a91dc403c4fdc2c9b147ce4dd3f56
Anhar <anhar_tasman@yahoo.com>
2024-04-09 14:07:07 +0700

    text

--- App.js
+++ App.js
@@ -4,21 +4,10 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     //flex : 1 willl make view occupy the entire screen
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <View
-        style={{
-          width: 200,
-          height: 200,
-          backgroundColor: "lightblue",
-        }}
-      ></View>
-      <View
-        style={{
-          width: 200,
-          height: 200,
-          backgroundColor: "lightgreen",
-        }}
-      ></View>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Text>
+        <Text style={{ color: "white" }}>Hello</Text> World
+      </Text>
     </View>
   );
 }
